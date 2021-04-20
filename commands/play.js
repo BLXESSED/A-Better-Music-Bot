fb28302ed0e19513b2d05bee811762f1511addd6
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-04-20 14:43:08 -0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -91,7 +91,6 @@ module.exports = {
             if (ytdl.validateURL(args[0])) {
                 const song_info = await ytdl.getInfo(args[0]);
                 song = { title: song_info.videoDetails.title, url: song_info.videoDetails.video_url, thumbnail: song_info.thumbnail_url }
-                console.log(song.thumbnail)
             } else {
                 const video_finder = async (query) =>{
                     const video_result = await ytSearch(query);
@@ -101,7 +100,6 @@ module.exports = {
                 const video = await video_finder(args.join(' '));
                 if (video){
                     song = { title: video.title, url: video.url, thumbnail: video.thumbnail_url }
-                    console.log(song.thumbnail)
                 } else {
                      message.channel.send(newEmbed4);
                 }
