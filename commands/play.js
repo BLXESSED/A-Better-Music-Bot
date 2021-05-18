ad151913d40d0cd5fba69755a20abb2feb45c1c2
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-05-18 10:38:48 -0700

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -123,7 +123,7 @@ module.exports = {
     
                 try {
                     const connection = await voice_channel.join();
-                    connection.voice.setSelfDeaf(true)
+                    voice_channel.setSelfDeaf(true)
                     queue_constructor.connection = connection;
                     video_player(message.guild, queue_constructor.songs[0], client);
                 } catch (err) {
