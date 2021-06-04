b5361c80a774b4192a27624ba91d0cb59417639a
BLXESSED <78892114+BLXESSED@users.noreply.github.com>
2021-06-03 17:16:14 -0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,8 +8,8 @@ module.exports = {
         .setTitle("Help")
         .setDescription("You can find all the commands on this panel. [Click here for more information](https://abettermusicbot.cf/)")
         .addFields(
-            {name: '<a:musicbeat:835354164782694440> **Music**', value: "`m!play, m!skip, m!stop, m!join, m!leave, m!nowplaying, m!pause, m!resume, m!forceskip, m!forcestop`"},
-            {name: '<a:gearSpinning:835354164547813396> **Utilities**', value: "`m!ping, m!invite, m!support, m!vote`"},
+            {name: '<a:musicbeat:835354164782694440> **Music**', value: "[Click Here](https://abettermusicbot.cf/commands/)"},
+            {name: '<a:gearSpinning:835354164547813396> **Utilities**', value: "[Click Here](https://abettermusicbot.cf/commands/)"},
             {name: '**Invite**', value: "Click [here](https://discord.com/oauth2/authorize?client_id=832063705021284362&scope=bot&permissions=573754624) to invite A Better Music Bot to your server"},
             {name: '**Support**', value: "Join our [support server](https://discord.gg/ctQezgBmcx) if you would like to see future announcements or just need some assistance using our discord bot"},
             {name: '**Vote**', value: "We'll really appreciate it if you can vote for our bot on [top.gg](https://top.gg/bot/832063705021284362/vote)"},
